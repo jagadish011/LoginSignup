852254be93a61492ee5871b4c206d9e34df76df1
jagadish011 <munavallijagadish@gmail.com>
2025-03-30 22:59:42 +0530

    signup and login in client

---
+++ client/loginSign/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+        <h1 className='text-3xl font-bold text-green-500 text-center'>hello react world</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
